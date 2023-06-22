ac0b7ede5f492048200fbc80c760678bc2f67629
root <root@DESKTOP-CM18J3T>
2023-06-22 09:53:28 +0300

    init commit

---
+++ merge.tf
@@ -0,0 +1,19 @@
+locals {
+    automated_path = "./variabels/automated.yaml"
+    manual_path = "./variabels/manual.yaml"
+    automated_only_path = "./variabels/automated_only.yaml"
+    
+    automated = fileexists(local.automated_path) ? file(local.automated_path) : yamlencode({})
+    manual = fileexists(local.manual_path) ? file(local.manual_path) : yamlencode({})
+    automated_only = fileexists(local.automated_only_path) ? file(local.automated_only_path) : yamlencode({})
+
+    settings = merge(
+        yamldecode(local.automated_only),
+        yamldecode(local.manual),
+        yamldecode(local.automated)
+    )
+}
+
+output "project_settings" {
+    value = local.settings
+}
